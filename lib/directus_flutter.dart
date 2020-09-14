bdb88dd97c3172de3dcfc2982c8d4de12dde2d97
Jan Storm <jstorm@die-netzwerkstatt.de>
2020-09-14 11:38:16 +0200

    [TASK] Renamed main entry point

--- lib/directus_flutter.dart
+++ lib/directus_flutter.dart
@@ -2,3 +2,4 @@ library directus_flutter_core;
 
 export 'src/core/directus_content_type_registry.dart';
 export 'src/core/abstract_directus_widget.dart';
+export 'src/repository/directus_application_repository.dart';
